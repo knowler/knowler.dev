79c9767b7e6b74a5c2c0f0aa7a8ff7d14ac9e947
Nathan Knowler <nathan@knowler.me>
2018-08-25 03:30:23 -0600

    Don't enqueue script since we aren't using anything

--- web/app/themes/sage/app/setup.php
+++ web/app/themes/sage/app/setup.php
@@ -12,7 +12,6 @@ use Roots\Sage\Template\BladeProvider;
  */
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 }, 100);
 
 add_action('wp_footer', function() {
