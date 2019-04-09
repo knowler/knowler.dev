8a3cc42d24d737e4a45008761b7e1bb3ce38599e
Nathan Knowler <nathan@knowler.me>
2019-04-09 16:01:51 -0600

    Convert to static site on Netlify (#26)

--- web/app/themes/sage/resources/views/partials/entry-meta.blade.php
+++
@@ -1,3 +0,0 @@
-<p class="text-right">
-  <time class="text-sm" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
-</p>
--- web/app/themes/sage/resources/views/partials/header.blade.php
+++
@@ -1,6 +0,0 @@
-<header class="flex flex-wrap p-3 sm:p-4 border-green-darkest border-b-8">
-  <div class="w-full md:w-1/2">
-    <h1 class="my-1 text-xl md:text-2xl lg:text-3xl"><a class="text-white no-underline" href="{{ home_url('/') }}">Nathan Knowler</a></h1>
-    <h2 class="my-1 text-lg md:text-xl lg:text-2xl">Web Developer &amp; Designer</h2>
-  </div>
-</header>
