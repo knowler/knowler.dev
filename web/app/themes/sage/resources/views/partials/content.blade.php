f61fb92643c79a652f19f77c94fa85adab164e10
Nathan Knowler <nathan@knowler.me>
2019-04-09 14:45:45 -0600

    Clean slate

--- web/app/themes/sage/resources/views/partials/content.blade.php
+++
@@ -1,9 +0,0 @@
-<article @php(post_class('Terminal'))>
-  <header>
-    <h2 class="mv4-l f2 f1-m f-subheadline-l"><a class="white-80 link dim" href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="lh-copy measure-wide">
-    @php(the_excerpt())
-  </div>
-</article>
