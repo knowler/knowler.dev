7d7fd14c20a8300070281dbc6954b18cfbd90dc9
Nathan Knowler <nathan@knowler.me>
2019-05-19 04:46:55 -0600

    Use Sapper for frontend (#34)

---
+++ src/components/Terminal.svelte
@@ -0,0 +1,14 @@
+<style>
+  .Terminal {
+    border: 1px solid var(--green);
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+    letter-spacing: 0.05em;
+    margin: 1rem;
+    max-width: 732px;
+    padding: 1rem;
+  }
+</style>
+
+<div class="Terminal">
+  <slot></slot>
+</div>
