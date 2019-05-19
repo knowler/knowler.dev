a907a65d97676e1112507efd03424e6d0780c184
Nathan Knowler <nathan@knowler.me>
2019-05-19 04:41:38 -0600

    Use Sapper for frontend

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,72 @@
-const path = require('path')
+const webpack = require('webpack')
+const config = require('sapper/config/webpack.js')
+const pkg = require('./package.json')
 
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
 
-module.exports = (env, argv) => ({
-  mode: argv && argv.mode ? argv.mode : 'development',
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    'index': './index',
+const common = {
+  mode,
+  resolve: {
+    extensions: [ '.mjs', '.js', '.json', '.svelte', '.html' ],
+    mainFields: [ 'svelte', 'module', 'browser', 'main' ],
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+}
+
+module.exports = {
+  client: {
+    ...common,
+    entry: config.client.entry(),
+    output: config.client.output(),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              hydratable: true,
+              hotReload: false,
+            },
+          },
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      })
+    ].filter(Boolean),
+    devtool: dev && 'inline-source-map',
   },
-  plugins: [
-    new FriendlyErrorsWebpackPlugin(),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      'index.html',
-      'app-icon.png',
-      'app-splash.png',
-      'favicon.png',
-      'manifest.json',
-      'sw.js',
-    ]),
-  ],
-})
+  server: {
+    ...common,
+    entry: config.server.entry(),
+    output: config.server.output(),
+    target: 'node',
+    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              css: false,
+              generate: 'ssr',
+            },
+          },
+        },
+      ],
+    },
+    performance: { hints: false },
+  },
+  serviceworker: {
+    mode,
+    entry: config.serviceworker.entry(),
+    output: config.serviceworker.output(),
+  },
+}
