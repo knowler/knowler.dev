65bd2115dae36b18997bc252424006354eea9590
Nathan Knowler <nathan@knowler.me>
2020-06-21 22:34:19 -0600

    fix: bump contrast scale
    The generated colours were 0.05 below the necessary
    contrast ratio.

--- colors.js
+++ colors.js
@@ -5,7 +5,7 @@ const base = '#e3fff9';
 const green = generateContrastColors({
   colorKeys: ['#00ffca'],
   base,
-  ratios: [1.5, 3, 4.5, 6, 7.5, 9, 10.5, 12, 13.5],
+  ratios: [1.55, 3.1, 4.65, 6.2, 7.75, 9.3, 10.85, 12.4, 13.95],
   colorspace: 'CAM02',
 });
 
