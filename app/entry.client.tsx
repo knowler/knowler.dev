8f928b4395286529aa026998b74ee5f0e6de6ce0
Nathan Knowler <nathan@knowler.me>
2022-04-10 13:15:04 -0500

    Fix hydrateRoot import + signature

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,4 @@
-import { hydrateRoot } from "react-dom";
+import { hydrateRoot } from "react-dom/client";
 import { RemixBrowser } from "@remix-run/react";
 
-hydrateRoot(<RemixBrowser />, document);
+hydrateRoot(document, <RemixBrowser />);
