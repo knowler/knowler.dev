3b88fac0641772b2fc8c0e965fc98de2a13bbab9
Nathan Knowler <nathan@knowler.me>
2022-04-17 22:29:08 -0500

    Install and use Octokit throttling plugin
    Need to look into the configuration

--- app/octokit.server.ts
+++ app/octokit.server.ts
@@ -1,5 +1,8 @@
 import { Octokit } from "octokit";
+import { throttling } from "@octokit/plugin-throttling";
 
-export const octokit = new Octokit({
+const OctokitWithThrottling = Octokit.plugin(throttling);
+
+export const octokit = new OctokitWithThrottling({
   auth: process.env.GITHUB_TOKEN as string,
 });
