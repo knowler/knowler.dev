81645e8787e892b6154f4d2e3755fc267b3ce913
Nathan Knowler <nathan@knowler.me>
2022-04-21 10:38:43 -0500

    Remove scripts again
    Was just using these for testing...

--- app/root.tsx
+++ app/root.tsx
@@ -92,8 +92,6 @@ export default function App() {
               </ul>
             </nav>
           </footer>
-          <ScrollRestoration />
-          <Scripts />
           <LiveReload />
         </body>
       </html>
