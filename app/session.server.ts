3181c251e74e08ef9af16b7e0abd76ccc2c301ee
Nathan Knowler <nathan@knowler.me>
2022-05-01 17:14:39 -0500

    Lengthen session max age

--- app/session.server.ts
+++ app/session.server.ts
@@ -1,16 +1,15 @@
 import { createCookieSessionStorage } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
-invariant(process.env.SESSION_DOMAIN, 'SESSION_DOMAIN must be set');
-invariant(process.env.SESSION_SECRET, 'SESSION_SECRET must be set');
+invariant(process.env.SESSION_DOMAIN, "SESSION_DOMAIN must be set");
+invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__session",
     domain: process.env.SESSION_DOMAIN as string,
-    expires: new Date(Date.now() + 60_000),
     httpOnly: true,
-    maxAge: 60,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
     path: "/",
     sameSite: "lax",
     secrets: [process.env.SESSION_SECRET as string],
