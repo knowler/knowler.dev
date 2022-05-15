cfb4d68d933d46c39afee64e4d9feac7542bc974
Nathan Knowler <nathan@knowler.me>
2022-05-15 15:02:14 -0500

    Do not allow logging out with GET

--- app/routes/__public/logout.tsx
+++ app/routes/__public/logout.tsx
@@ -1,5 +1,4 @@
-import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
 import { auth } from "~/auth.server";
 
 export const action: ActionFunction = async ({request}) => await auth.logout(request, { redirectTo: '/' });
-export const loader: LoaderFunction = async ({request}) => await auth.logout(request, { redirectTo: '/' });
