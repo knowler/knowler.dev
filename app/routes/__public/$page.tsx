fab88d41cc8d4722b02e0469c521f131c1a93d37
Nathan Knowler <nathan@knowler.me>
2022-11-04 22:35:10 -0500

    Remove edit link for pages

--- app/routes/__public/$page.tsx
+++ app/routes/__public/$page.tsx
@@ -1,7 +1,6 @@
 import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { useAuthenticated } from "~/hooks";
+import { useLoaderData } from "@remix-run/react";
 import { prisma } from "~/db.server";
 import { notFound } from "remix-utils";
 import { getSeoMeta } from "~/seo";
@@ -26,25 +25,11 @@ export const loader: LoaderFunction = async ({ params }) => {
 
 export default function Page() {
   const { page } = useLoaderData<typeof loader>();
-  const isAuthenticated = useAuthenticated();
 
   return (
-    <>
-      <article
-        className="prose"
-        dangerouslySetInnerHTML={{ __html: page.html }}
-      />
-      <aside className="edit-link">
-        {isAuthenticated ? (
-          <Link to={`/admin/pages/edit/${page.id}`}>Edit this page</Link>
-        ) : (
-          <a
-            href={`https://github.com/knowler/knowler.dev/blob/main/content/pages/${page.slug}.md`}
-          >
-            Edit on GitHub
-          </a>
-        )}
-      </aside>
-    </>
+		<article
+			className="prose"
+			dangerouslySetInnerHTML={{ __html: page.html }}
+		/>
   );
 }
