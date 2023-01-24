3b6f2d095a550479dd228fea045d0e0561f6999c
Nathan Knowler <nathan@knowler.me>
2023-01-24 00:21:10 -0600

    Add date to list

--- app/routes/__public/index.tsx
+++ app/routes/__public/index.tsx
@@ -2,12 +2,13 @@ import { json, LoaderArgs, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { prisma } from "~/db.server";
 import { getSeoMeta } from "~/seo";
+import { winterpegDateTime } from "~/utils";
 
 export const meta: MetaFunction = () => getSeoMeta({
-  title: "Home",
+	title: "Home",
 });
 
-export async function loader({}: LoaderArgs) {
+export async function loader({ }: LoaderArgs) {
 	const latestPosts = await prisma.post.findMany({
 		where: { published: true },
 		orderBy: { publishedAt: "desc" },
@@ -28,9 +29,9 @@ export async function loader({}: LoaderArgs) {
 }
 
 export default function Index() {
-	const {webmentions, latestPosts} = useLoaderData<typeof loader>();
+	const { webmentions, latestPosts } = useLoaderData<typeof loader>();
 
-  return (
+	return (
 		<article className="h-entry prose has-links">
 			<h1>I am Nathan Knowler</h1>
 			<p>
@@ -41,11 +42,12 @@ export default function Index() {
 			{latestPosts ? (
 				<section>
 					<h2>Latest blog posts</h2>
-					<ol reversed>
+					<ol reversed role="list" style={{ listStyle: 'none', paddingInlineStart: 0 }}>
 						{latestPosts.map(post => (
 							<li key={post.id}>
 								<article>
 									<h3><a href={`/blog/${post.slug}`} rel="bookmark">{post.title}</a></h3>
+									<p><time dateTime={post.publishedAt}>{new Date(post.publishedAt).toDateString()}</time></p>
 									<p>{post.description}</p>
 								</article>
 							</li>
@@ -58,14 +60,14 @@ export default function Index() {
 				<aside aria-labelledby="webmentionsLabel">
 					<details>
 						<summary>{webmentions.length} <span id="webmentionsLabel">Webmention{webmentions.length > 1 ? "s" : ""}</span></summary>
-							<ol>
-								{webmentions.map(webmention => (
-									<li key={webmention.id}><a href={webmention.source}>“{webmention.title}” by {webmention.author}</a></li>
-								))}
-							</ol>
+						<ol>
+							{webmentions.map(webmention => (
+								<li key={webmention.id}><a href={webmention.source}>“{webmention.title}” by {webmention.author}</a></li>
+							))}
+						</ol>
 					</details>
 				</aside>
 			) : null}
 		</article>
-  );
+	);
 }
