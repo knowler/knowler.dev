419eae51f4208a0be3df2bd550d03dd1a0c3fb94
Nathan Knowler <nathan@knowler.dev>
2025-02-04 12:52:30 -0600

    Do not bust posts object cache on deployment

--- models/posts.js
+++ models/posts.js
@@ -6,7 +6,7 @@ export class Posts {
 	}
 
 	get cache() {
-		const cache = caches.open(`${Deno.env.get("DENO_DEPLOYMENT_ID")}.posts.${Deno.env.get("CONTENT_VERSION")}`);
+		const cache = caches.open(`posts.${Deno.env.get("CONTENT_VERSION")}`);
 
 		return {
 			async match(url) {
@@ -38,7 +38,7 @@ export class Posts {
 		const { value: id } = await this.kv.get(["postsBySlug", slug]);
 		if (!id) throw `Post not found with slug: ${slug}`;
 		const post = await this.getById(id);
-		response = new Response(new Blob([JSON.stringify(post)], { type: "application/json" }));
+		response = Response.json(post);
 		await this.cache.put(url, response);
 
 		return post;
@@ -55,7 +55,7 @@ export class Posts {
 
 		const { value: post } = await this.kv.get(["posts", id]);
 		if (!post) throw `Post not found with id: ${id}`;
-		response = new Response(new Blob([JSON.stringify(post)], { type: "application/json" }));
+		response = Response.json(post);
 		// Check if the slug version has been cached? If not, cache a clone.
 		const slugURL = this.slugURL(post.slug);
 		if (!this.cache.match(slugURL)) {
@@ -70,10 +70,9 @@ export class Posts {
 		const matched = await this.cache.match(new URL(`/id/${post.id}`, this.baseURL));
 		if (matched) return;
 
-		let blob = new Blob([JSON.stringify(post)], { type: "application/json" });
 		return Promise.all([
-			this.cache.put(this.slugURL(post.slug), new Response(blob)),
-			this.cache.put(this.idURL(post.id), new Response(blob)),
+			this.cache.put(this.slugURL(post.slug), Response.json(post)),
+			this.cache.put(this.idURL(post.id), Response.json(post)),
 		]);
 	}
 }
