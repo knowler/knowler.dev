0971a70a87e978c146e646962707d5e3af503511
Nathan Knowler <nathan@knowler.dev>
2025-09-05 14:58:00 -0500

    Use one script for both site-header and site-footer

--- experimental/minimal-templating/site-layout-elements.js
+++ experimental/minimal-templating/site-layout-elements.js
@@ -1,3 +1,37 @@
+customElements.define("site-header", class extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: "open" });
+		this.shadowRoot.innerHTML = `
+			<style>
+				ul {
+					display: flex;
+					list-style: none;
+					padding-inline-start: 0;
+					margin-block: 0;
+					gap: 16px;
+				}
+				
+				a[aria-current="page"] {
+					text-decoration: none;
+				}
+			</style>
+			<span>Nathan Knowler</span>
+			<nav>
+				<ul>
+					<li><a href=/>Welcome</a>
+					<li><a href=/about>About</a>
+					<li><a href=/blog>Blog</a>
+					<li><a href=/uses>Uses</a>
+				</ul>
+			</nav>
+		`;
+		for (const link of this.shadowRoot.querySelectorAll(":any-link")) {
+			if (link.pathname === location.pathname) link.ariaCurrent = "page";
+		}
+	}
+});
+
 customElements.define("site-footer", class extends HTMLElement {
 	constructor() {
 		super();
