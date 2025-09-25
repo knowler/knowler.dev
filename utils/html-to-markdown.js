9afcaf29831f809b881b81ef0b7e026ba8e30e36
Nathan Knowler <nathan@knowler.dev>
2025-09-24 22:18:06 -0500

    Add note and label directives for markdown editing

--- utils/html-to-markdown.js
+++ utils/html-to-markdown.js
@@ -1,6 +1,9 @@
 import { unified } from "npm:unified";
 import { toHtml } from "npm:hast-util-to-html";
+import { toMdast } from "npm:hast-util-to-mdast";
+import { toText } from "npm:hast-util-to-text";
 import remarkStringify from "npm:remark-stringify";
+import remarkDirective from "npm:remark-directive";
 import remarkReferenceLinks from "npm:remark-reference-links";
 import rehypeParse from "npm:rehype-parse";
 import rehypeRemark from "npm:rehype-remark";
@@ -35,9 +38,46 @@ export function htmlToMarkdown(html) {
 					state.patch(node, result);
 					return result;
 				},
-				aside(state, node) {
+				time(state, node) {
 					const result = { type: "html", value: toHtml(node) };
 					state.patch(node, result);
+					return result;
+				},
+				aside(state, node) {
+					let result;
+					if (node.properties.role === "note") {
+						const { role, ariaLabelledBy, ...attributes } = node.properties;
+						const [labelId] = ariaLabelledBy;
+						result = {
+							name: "note",
+							type: "containerDirective",
+							attributes,
+							children: toMdast({ type: "root", children: node.children }, {
+								handlers: {
+									...state.options.handlers,
+									strong(state, node) {
+										if (node.properties?.id === labelId) {
+											const { id, ...attributes } = node.properties;
+											const result = {
+												name: "label",
+												type: "textDirective",
+												attributes,
+												children: [
+													{ type: "text", value: toText({ type: "root", children: node.children }) },
+												],
+											};
+											state.patch(node, result);
+											return result;
+										}
+									},
+								},
+							}).children,
+						};
+						state.patch(node, result);
+					} else {
+						result = { type: "html", value: toHtml(node) };
+						state.patch(node, result);
+					}
 					return result;
 				},
 				iframe(state, node) {
@@ -47,6 +87,7 @@ export function htmlToMarkdown(html) {
 				},
 			},
 		})
+		.use(remarkDirective)
 		.use(remarkReferenceLinks)
 		.use(remarkStringify)
 		.process(html);
--- utils/markdown-to-html.js
+++ utils/markdown-to-html.js
@@ -1,16 +1,59 @@
 import { unified } from "npm:unified";
 import remarkParse from "npm:remark-parse";
 import remarkRehype from "npm:remark-rehype";
+import remarkDirective from "npm:remark-directive";
 import rehypeHighlight from "npm:rehype-highlight";
 import rehypeSlug from "npm:rehype-slug";
 import rehypeStringify from "npm:rehype-stringify";
+import {visit} from "npm:unist-util-visit"
+import {h} from "npm:hastscript"
+import { toKebabCase } from "@std/text";
 
 export function markdownToHTML(markdown) {
 	return unified()
 		.use(remarkParse)
+		.use(remarkDirective)
+		.use(noteDirective)
 		.use(remarkRehype, { allowDangerousHtml: true })
 		.use(rehypeHighlight)
 		.use(rehypeSlug)
 		.use(rehypeStringify, { allowDangerousHtml: true })
 		.process(markdown);
+}
+
+function noteDirective() {
+	const labelsWithinNotes = new WeakSet();
+	return function(tree) {
+		visit(tree, function(node) {
+			if (node.type === "containerDirective" && node.name === "note") {
+				const data = node.data || (node.data = {});
+				const hast = h(node.name, node.attributes ?? {});
+
+				data.hName = "aside";
+				data.hProperties = { role: "note", ...hast.properties };
+
+				const [firstChild] = node.children;
+
+				if (firstChild.type === "paragraph") {
+					const [label] = firstChild.children.filter(
+						child => child.type === "textDirective" && child.name === "label",
+					);
+					if (label && label.children.length === 1 && label.children[0].type === "text") {
+						labelsWithinNotes.add(label)
+
+						const id = toKebabCase(label.children[0].value);
+						data.hProperties.ariaLabelledBy = id;
+					}
+				}
+			}
+
+			if (node.type === "textDirective" && node.name === "label" && labelsWithinNotes.has(node)) {
+				const data = node.data || (node.data = {});
+				const hast = h(node.name, node.attributes ?? {});
+
+				data.hName = "strong";
+				data.hProperties = { id: toKebabCase(node.children[0].value), ...hast.properties };
+			}
+		});
+	}
 }
