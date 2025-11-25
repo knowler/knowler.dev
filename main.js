a393f9bcbf14439cc65211c8f20ca695702e6a0c
Nathan Knowler <nathan@knowler.dev>
2025-11-25 16:31:28 -0600

    Make anti-js js club banner immutable

--- main.js
+++ main.js
@@ -207,6 +207,10 @@ app.use(
 		wait: true,
 	}),
 );
+app.get("/banners/anti-javascript-javascript-club-member-88x31.png", (c, next) => {
+	c.header("cache-control", "public, max-age=31536000, immutable");
+	next();
+});
 app.use("*", serveStatic({ root: "./assets" }));
 
 Deno.serve({ port: ENV === "production" ? 8000 : new URL(SITE_URL).port }, app.fetch);
