df71e250e6f859517b3975534bce5e666fd810d4
Nathan Knowler <nathan@knowler.dev>
2025-11-26 10:32:59 -0600

    Use Font Awesome Pro package

--- web/middleware/pug-renderer.js
+++ web/middleware/pug-renderer.js
@@ -5,8 +5,7 @@ import { invariant } from "@knowler/shared/invariant";
 import { isCSSNakedDay } from "~/utils/is-css-naked-day.js";
 import { library, icon, findIconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-//import { far } from "@fortawesome/pro-regular-svg-icons";
+import { fas } from "@fortawesome/pro-solid-svg-icons";
 
 const SITE_URL = Deno.env.get("SITE_URL");
 invariant(SITE_URL);
