a34415a1354a54c1cd3a8759a835f5a7079b1ab3
Nathan Knowler <nathan@knowler.dev>
2025-11-26 23:47:57 -0600

    Begin adding build for CSS

---
+++ web/tools/build-css.js
@@ -0,0 +1,15 @@
+import { bundle, browserslistToTargets, Features } from "lightningcss";
+import browserslist from "npm:browserslist";
+
+const DENO_DEPLOY_BUILD_ID = Deno.env.get("DENO_DEPLOY_BUILD_ID");
+
+let { code } = bundle({
+	filename: "./src/main.css",
+	targets: browserslistToTargets(browserslist("baseline widely available")),
+	include: Features.Nesting,
+	exclude: Features.Colors,
+});
+
+console.log(DENO_DEPLOY_BUILD_ID);
+
+await Deno.writeFile(`./assets/main.${DENO_DEPLOY_BUILD_ID}.css`, code);
