f0c7578a71e036e031c24c3a89c0f2b94a45d4e9
Nathan Knowler <nathan@knowler.dev>
2025-11-27 00:18:19 -0600

    Fix build?

--- web/tools/build-css.js
+++ web/tools/build-css.js
@@ -1,15 +1,13 @@
-import { bundle, browserslistToTargets, Features } from "lightningcss";
+import { bundleAsync as bundle, browserslistToTargets, Features } from "lightningcss"
 import browserslist from "browserslist";
 
 const DENO_DEPLOY_BUILD_ID = Deno.env.get("DENO_DEPLOY_BUILD_ID");
 
-let { code } = bundle({
+let { code } = await bundle({
 	filename: "./src/main.css",
 	targets: browserslistToTargets(browserslist("baseline widely available")),
 	include: Features.Nesting,
 	exclude: Features.Colors,
 });
-
-console.log(DENO_DEPLOY_BUILD_ID);
 
 await Deno.writeFile(`./assets/main.${DENO_DEPLOY_BUILD_ID}.css`, code);
