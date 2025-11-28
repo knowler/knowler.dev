dfe1fa05f092acfab1a8023c92a54615c4884da1
Nathan Knowler <nathan@knowler.dev>
2025-11-28 09:46:28 -0600

    Removing unused stuff

--- web/tools/utils/bust-content-cache.js
+++
@@ -1,6 +0,0 @@
-import { kv } from "./production-kv.js";
-
-// Bust the content cache
-const { value: cache_versions } = await kv.get(["cache_versions"]);
-cache_versions.content_version = Date.now();
-await kv.set(["cache_versions"], cache_versions);
--- web/tools/utils/production-kv.js
+++
@@ -1,4 +0,0 @@
-const DENO_KV_DB_UUID = Deno.env.get("DENO_KV_DB_UUID");
-export const kv = await Deno.openKv(
-	`https://api.deno.com/databases/${DENO_KV_DB_UUID}/connect`,
-);
