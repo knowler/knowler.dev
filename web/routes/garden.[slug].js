b93c8ab8fe590eaeb8b40b6855fe59fbe6584c3e
Nathan Knowler <nathan@knowler.dev>
2025-12-08 12:34:28 -0600

    Add support for titles in garden content

--- web/routes/garden.[slug].js
+++ web/routes/garden.[slug].js
@@ -1,3 +1,5 @@
+import { parseHTML } from "npm:linkedom";
+
 export async function get(c, next) {
 	try {
 		const params = c.req.param();
@@ -6,7 +8,10 @@ export async function get(c, next) {
 		const { value: content } = await kv.get(["garden", id]);
 
 		return c.render("garden.[slug]", {
+			title: parseHTML(`<h1>${content.title}</h1>`)?.document.querySelector("h1")?.textContent ?? content.title ?? "Untitled note",
+			headline: content.title, 
 			content,
+			canonical: trimTrailingSlash(c.req.url),
 		});
 	} catch (e) {
 		console.error(e);
