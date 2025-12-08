65f79dbfc8016fb3ccecd0d987ccf562accb7299
Nathan Knowler <nathan@knowler.dev>
2025-12-08 14:50:28 -0600

    Add prettyUpdatedAt to the locals correctly

--- web/routes/garden.[slug].js
+++ web/routes/garden.[slug].js
@@ -9,15 +9,13 @@ export async function get(c, next) {
 		const { value: id } = await kv.get(["gardenBySlug", params.slug]);
 		const { value: content } = await kv.get(["garden", id]);
 
-		if (content.updatedAt)
-			content.prettyUpdatedAt = winnipegDateTime(new Date(content.updatedAt));
-
 		return c.render("garden.[slug]", {
 			title: parseHTML(`<h1>${content.title}</h1>`)?.document.querySelector("h1")?.textContent ?? content.title ?? "Untitled note",
 			headline: content.title, 
 			content,
 			canonical: trimTrailingSlash(c.req.url),
 			prettyCreatedAt: winnipegDateTime(new Date(content.createdAt)),
+			prettyUpdatedAt: content.updatedAt ? winnipegDateTime(new Date(content.updatedAt)) : undefined,
 		});
 	} catch (e) {
 		console.error(e);
